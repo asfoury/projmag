91c0061e0b496658fe337e5eb7b13b34bcc613eb
asfoury <mohamed.elasfoury@epfl.ch>
2021-03-11 18:38:41 +0100

    Implemented the projects list view

--- app/src/main/java/com/sdp13epfl2021/projmag/MainActivity.kt
+++ app/src/main/java/com/sdp13epfl2021/projmag/MainActivity.kt
@@ -2,10 +2,18 @@ package com.sdp13epfl2021.projmag
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.sdp13epfl2021.projmag.adapter.ItemAdapter
+import com.sdp13epfl2021.projmag.data.Datasource
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val myDataset = Datasource().loadProjects()
+        val recyclerView = findViewById<RecyclerView>(R.id.recycler_view)
+        recyclerView.adapter = ItemAdapter(this, myDataset)
+        recyclerView.setHasFixedSize(true)
     }
 }
---
+++ app/src/main/java/com/sdp13epfl2021/projmag/adapter/ItemAdapter.kt
@@ -0,0 +1,53 @@
+package com.sdp13epfl2021.projmag.adapter
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.LinearLayout
+import android.widget.TextView
+import androidx.core.view.marginRight
+import androidx.core.view.setPadding
+import androidx.recyclerview.widget.RecyclerView
+import com.google.android.material.chip.Chip
+import com.sdp13epfl2021.projmag.R
+import com.sdp13epfl2021.projmag.model.Project
+
+class ItemAdapter(private val context: Context, private val dataset : List<Project>) : RecyclerView.Adapter<ItemAdapter.ItemViewHolder>() {
+
+    class ItemViewHolder(private val view : View) : RecyclerView.ViewHolder(view) {
+        val textView : TextView = view.findViewById(R.id.project_title)
+        val labNameView : TextView = view.findViewById(R.id.lab_name)
+        val linearLayoutView : LinearLayout = view.findViewById(R.id.linear_layout_2)
+
+
+    }
+
+
+    override fun getItemCount() =  dataset.size
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ItemViewHolder {
+      val adapterLayout = LayoutInflater.from(parent.context)
+              .inflate(R.layout.list_project, parent, false)
+        return ItemViewHolder(adapterLayout)
+    }
+
+
+    override fun onBindViewHolder(holder: ItemViewHolder, position: Int) {
+        val project = dataset[position]
+        // set the project name
+        holder.textView.text = project.name.toString()
+        // set the lab name
+        holder.labNameView.text = project.lab
+        // add the tags to the project
+       for(tag in project.tags) {
+           val chipView : Chip = Chip(context)
+           chipView.text = tag
+
+           holder.linearLayoutView.addView(chipView)
+       }
+
+
+    }
+
+}
