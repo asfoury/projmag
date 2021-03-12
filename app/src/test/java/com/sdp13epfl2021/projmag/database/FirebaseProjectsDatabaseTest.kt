7814a5eee4c29caed5e75441fe1adcdccbe4df55
Paul Coral <paul.coral@epfl.ch>
2021-03-12 17:05:14 +0100

    Add unit test for FirebaseProjectsDatabase
    Unit test are simple because the code strongly rely on Firebase API

---
+++ app/src/test/java/com/sdp13epfl2021/projmag/database/FirebaseProjectsDatabaseTest.kt
@@ -0,0 +1,19 @@
+package com.sdp13epfl2021.projmag.database
+
+import org.junit.Test
+
+/**
+ *  Simple tests as this is just an abstraction of the
+ *  Firebase/Firestore API
+ */
+class FirebaseProjectsDatabaseTest {
+    /**
+     * test that no unexpected behaviour when passing null
+     */
+    @Test
+    fun pushNullProjectShouldNotCrash(){
+        FirebaseProjectsDatabase.pushProjectWithId(null,
+            {},
+            {})
+    }
+}
