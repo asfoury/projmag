0f8f32352bd6b01f766ab3aa5398ff1fa278f195
arnauds5 <arnauds5@users.noreply.github.com>
2021-03-15 17:03:20 +0100

    Cleaning CachedProjectsDatabase

--- app/src/main/java/com/sdp13epfl2021/projmag/database/CachedProjectsDatabase.kt
+++ app/src/main/java/com/sdp13epfl2021/projmag/database/CachedProjectsDatabase.kt
@@ -13,7 +13,7 @@ class CachedProjectsDatabase(private val db: ProjectsDatabase) : ProjectsDatabas
 
     init {
         db.addProjectsChangeListener { change ->
-            when(change.type) {
+            when (change.type) {
                 ProjectChange.Type.ADDED -> addProject(change.project)
                 ProjectChange.Type.MODIFIED -> addProject(change.project)
                 ProjectChange.Type.REMOVED -> change.project?.let { removeProjectWithId(it.id) }
@@ -29,10 +29,11 @@ class CachedProjectsDatabase(private val db: ProjectsDatabase) : ProjectsDatabas
     /**
      * Add a Project to the local cache.
      * If the cache contains a project with the same id,
-     * the local project is removed and replaced by this one.
+     * the local project is removed and replaced by the new one.
      * If the project is null, nothing is done.
      */
-    @Synchronized private fun addProject(project: Project) {
+    @Synchronized
+    private fun addProject(project: Project) {
         project?.let {
             removeProjectWithId(it.id)
             projects = projects + it
@@ -44,13 +45,15 @@ class CachedProjectsDatabase(private val db: ProjectsDatabase) : ProjectsDatabas
      * Remove a project from its id, from the local cache.
      * If the project is not present, nothing is done.
      */
-    @Synchronized private fun removeProjectWithId(id: ProjectId) {
+    @Synchronized
+    private fun removeProjectWithId(id: ProjectId) {
         projects = projects.filter { p -> p!!.id != id }
     }
 
 
     /**
      * Synchronously get all projects cached locally.
+     * This list is guaranteed to have only non null Project.
      */
     fun getAllProjects(): List<Project> {
         return projects
@@ -58,11 +61,11 @@ class CachedProjectsDatabase(private val db: ProjectsDatabase) : ProjectsDatabas
 
 
     override fun getAllIds(onSuccess: (List<ProjectId>) -> Unit, onFailure: (Exception) -> Unit) {
-        GlobalScope.launch { onSuccess(projects.mapNotNull{p -> p!!.id }) }
+        GlobalScope.launch { onSuccess(projects.mapNotNull { p -> p!!.id }) }
     }
 
     override fun getProjectFromId(id: ProjectId, onSuccess: (Project) -> Unit, onFailure: (Exception) -> Unit) {
-        GlobalScope.launch { onSuccess(projects.find { p -> p!!.id == id}) }
+        GlobalScope.launch { onSuccess(projects.find { p -> p!!.id == id }) }
     }
 
     override fun getAllProjects(onSuccess: (List<Project>) -> Unit, onFailure: (Exception) -> Unit) {
@@ -70,13 +73,15 @@ class CachedProjectsDatabase(private val db: ProjectsDatabase) : ProjectsDatabas
     }
 
     override fun getProjectsFromName(name: String, onSuccess: (List<Project>) -> Unit, onFailure: (Exception) -> Unit) {
-        GlobalScope.launch { onSuccess(projects.filter { p -> p!!.name == name}) }
+        GlobalScope.launch { onSuccess(projects.filter { p -> p!!.name == name }) }
     }
 
     override fun getProjectsFromTags(tags: List<String>, onSuccess: (List<Project>) -> Unit, onFailure: (Exception) -> Unit) {
-        val listOfTags = tags.map { tag -> tag.toLowerCase(Locale.ROOT) }
         GlobalScope.launch {
-            onSuccess(projects.filter { p -> p!!.tags.any{ tag -> listOfTags.contains(tag.toLowerCase(Locale.ROOT))} })
+            val listOfTags = tags.map { tag -> tag.toLowerCase(Locale.ROOT) }
+            onSuccess(projects.filter {
+                p -> p!!.tags.any { tag -> listOfTags.contains(tag.toLowerCase(Locale.ROOT)) } }
+            )
         }
     }
 
@@ -88,7 +93,8 @@ class CachedProjectsDatabase(private val db: ProjectsDatabase) : ProjectsDatabas
         db.deleteProjectWithId(id, onSuccess, onFailure)
     }
 
-    @Synchronized override fun addProjectsChangeListener(changeListener: (ProjectChange) -> Unit) {
+    @Synchronized
+    override fun addProjectsChangeListener(changeListener: (ProjectChange) -> Unit) {
         listeners = listeners + changeListener
     }
 
