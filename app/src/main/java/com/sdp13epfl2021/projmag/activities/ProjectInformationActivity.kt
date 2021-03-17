0335a70d6a48559b14f811cc4e4dbf7d98d5a393
asfoury <mohamed.elasfoury@epfl.ch>
2021-03-17 07:35:52 +0100

    Projects are now pressable and open a new activity with their information and user can go back to the list

---
+++ app/src/main/java/com/sdp13epfl2021/projmag/activities/ProjectInformationActivity.kt
@@ -0,0 +1,11 @@
+package com.sdp13epfl2021.projmag.activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ProjectInformationActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_project_information)
+    }
+}
