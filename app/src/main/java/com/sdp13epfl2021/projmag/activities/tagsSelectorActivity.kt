82f7cf18d991570c3912975a090ac0e730971902
kaourintin <kaourintin.tamine.personal@gmail.com>
2021-04-02 03:05:54 +0200

    create tag activity, create xml for one tag, start doing adapter

---
+++ app/src/main/java/com/sdp13epfl2021/projmag/activities/tagsSelectorActivity.kt
@@ -0,0 +1,12 @@
+package com.sdp13epfl2021.projmag.activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.sdp13epfl2021.projmag.R
+
+class tagsSelectorActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_tags_selector)
+    }
+}
---
+++ app/src/main/java/com/sdp13epfl2021/projmag/adapter/TagAdapter.kt
@@ -0,0 +1,5 @@
+package com.sdp13epfl2021.projmag.adapter
+
+class TagAdapter {
+    
+}
