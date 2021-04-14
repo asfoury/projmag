967c991c65025ec24cc60e3a20ce6e255f17611d
Paul Coral <paul.coral@epfl.ch>
2021-04-14 19:33:08 +0200

    Add the ability to create WEBVTT subtitle format

---
+++ app/src/main/java/com/sdp13epfl2021/projmag/video/SubtitleBuilder.kt
@@ -0,0 +1,68 @@
+package com.sdp13epfl2021.projmag.video
+
+import java.util.concurrent.TimeUnit
+
+class SubtitleBuilder {
+    companion object {
+        const val START = true
+        const val END = !START
+
+        const val WEBVTT = "WEBVTT"
+
+        fun Int.webvttTime(): String {
+            val sec = (TimeUnit.MILLISECONDS.toSeconds(this.toLong()) % 60)
+                .toString()
+                .padStart(2, '0')
+            val min = (TimeUnit.MILLISECONDS.toMinutes(this.toLong()) % 60)
+                .toString()
+                .padStart(2, '0')
+            val hour = TimeUnit.MILLISECONDS.toHours(this.toLong())
+                .toString()
+                .padStart(2, '0')
+            val millis = (this % 1000)
+                .toString()
+                .padStart(3, '0')
+            return "$hour:$min:$sec.$millis"
+        }
+    }
+
+    var start: Int = 0
+        private set
+    var end: Int = 0
+        private set
+
+    private val subtitle: MutableList<SubtitlePart> = mutableListOf()
+
+    fun setStartOrEnd(or: Boolean, pos: Int) {
+        if (or == START) {
+            start = pos
+            end = if (end < pos) pos else end
+        } else {
+            start = if (pos < start) pos else start
+            end = pos
+        }
+    }
+
+    fun add(content: String) {
+        subtitle.add(SubtitlePart(content, start, end))
+    }
+
+    fun clear() {
+        subtitle.clear()
+    }
+
+    fun build(): String {
+        val str = "$WEBVTT\n\n"
+        return subtitle.fold(str) { s, e -> s + (e.toString()) }
+    }
+
+    private data class SubtitlePart(
+        val content: String,
+        val start: Int,
+        val end: Int
+    ) {
+        override fun toString(): String {
+            return "${start.webvttTime()} --> ${end.webvttTime()}\n$content\n\n"
+        }
+    }
+}
--- app/src/main/java/com/sdp13epfl2021/projmag/video/VideoSubtitlingActivity.kt
+++ app/src/main/java/com/sdp13epfl2021/projmag/video/VideoSubtitlingActivity.kt
@@ -1,36 +1,42 @@
 package com.sdp13epfl2021.projmag.video
 
+import android.media.MediaFormat
 import android.net.Uri
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
-import android.widget.MediaController
-import android.widget.TextView
-import android.widget.Toast
-import android.widget.VideoView
+import android.widget.*
+import androidx.appcompat.app.AppCompatActivity
 import com.sdp13epfl2021.projmag.FORM_TO_SUBTITLE_MESSAGE
 import com.sdp13epfl2021.projmag.R
+import com.sdp13epfl2021.projmag.video.SubtitleBuilder.Companion.webvttTime
 import org.w3c.dom.Text
+import java.util.*
 
 class VideoSubtitlingActivity : AppCompatActivity() {
-    companion object {
-        const val START = true
-        const val END = true
-    }
+
 
     private var videoUri: Uri? = null
+
+    private var builder = SubtitleBuilder()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_video_subtitling)
 
         videoUri = intent.getStringExtra(FORM_TO_SUBTITLE_MESSAGE)?.let { Uri.parse(it) }
-        handleVideoUri(videoUri)
+        findViewById<Button>(R.id.video_subtitling_set_start_button)?.setOnClickListener {
+            setStartOrEnd(SubtitleBuilder.START)
+        }
+        findViewById<Button>(R.id.video_subtitling_set_end_button)?.setOnClickListener {
+            setStartOrEnd(SubtitleBuilder.END)
+        }
+        findViewById<Button>(R.id.video_subtitling_add)?.setOnClickListener { addButtonPressed() }
+        handleVideoUri()
+        updateTimeTextView()
     }
 
-    fun handleVideoUri(uri: Uri?) {
+    private fun handleVideoUri() {
         if (videoUri == null) {
-            Log.d("video-sub", "if")
             Toast.makeText(
                 this,
                 getString(R.string.video_subtitling_cant_open_video),
@@ -38,7 +44,6 @@ class VideoSubtitlingActivity : AppCompatActivity() {
             ).show()
             finish()
         } else {
-            Log.d("video-sub", "else")
             val mediaController = MediaController(this)
             val videoView = findViewById<VideoView>(R.id.video_subtitling_videoview)
 
@@ -46,18 +51,40 @@ class VideoSubtitlingActivity : AppCompatActivity() {
             mediaController.setMediaPlayer(videoView)
             videoView.setVideoURI(videoUri)
             videoView.setMediaController(mediaController)
+            //val sub = "WEBVTT\n\n00:00:00.800 --> 00:00:04.115\nSubtitling Text".byteInputStream()
             videoView.start()
         }
     }
 
-    fun setStartOrEnd(or: Boolean) {
-        val videoView = findViewById<VideoView>(R.id.video_subtitling_videoview)
-        val textView: TextView = if (or)
-            findViewById(R.id.video_subtitling_start_time)
-        else
-            findViewById(R.id.video_subtitling_end_time)
-        videoView.suspend()
-        textView.text = videoView.currentPosition.toString()
+    private fun updateTimeTextView() {
+        findViewById<TextView>(R.id.video_subtitling_start_time)?.apply {
+            text = builder.start.webvttTime()
+        }
+        findViewById<TextView>(R.id.video_subtitling_end_time)?.apply {
+            text = builder.end.webvttTime()
+        }
+    }
 
+    private fun addButtonPressed() =
+        findViewById<EditText>(R.id.video_subtitling_subtitle_text)?.let {
+            builder.add(it.text.toString())
+            Log.d("test", builder.build())
+            findViewById<VideoView>(R.id.video_subtitling_videoview)?.let { vv ->
+                Log.d("test", builder.build())
+                vv.addSubtitleSource(
+                    builder.build().byteInputStream(),
+                    MediaFormat.createSubtitleFormat("text/vtt", Locale.ENGLISH.language)
+                )
+                vv.start()
+            }
+        }
+
+
+    private fun setStartOrEnd(or: Boolean) {
+        findViewById<VideoView>(R.id.video_subtitling_videoview)?.let { videoView ->
+            videoView.pause()
+            builder.setStartOrEnd(or, videoView.currentPosition)
+            updateTimeTextView()
+        }
     }
 }
