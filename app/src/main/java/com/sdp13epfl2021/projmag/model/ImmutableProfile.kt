8f7c2aed23b05a15596b0e7105085ab6760a0029
Giovanni Michel Igowa <giovanni.igowa@epfl.ch>
2021-04-15 12:48:44 +0200

    delete duplicate , add enummand sciper nullable

--- app/src/main/java/com/sdp13epfl2021/projmag/model/ImmutableProfile.kt
+++ app/src/main/java/com/sdp13epfl2021/projmag/model/ImmutableProfile.kt
@@ -2,7 +2,7 @@ package com.sdp13epfl2021.projmag.model
 
 
 class ImmutableProfile private constructor(val lastName : String, val firstName : String,
-                                           val age : Int, val gender : Gender, val sciper : Int, val phoneNumber : String) {
+                                           val age : Int, val gender : Gender, val sciper : Int?, val phoneNumber : String, val role : Role) {
 
     companion object{
         private const val MAX_LAST_NAME_SIZE = 40
@@ -21,18 +21,19 @@ class ImmutableProfile private constructor(val lastName : String, val firstName 
          * @param gender : gender on the profile
          * @param sciper : sciper on the profile
          * @param phoneNumber : phone number on the profile
+         * @param role : role on the profile
          * @return Success(profile) or failure(String explanation)
          */
         fun build(lastName : String, firstName : String,
-                  age : Int, gender : Gender, sciper : Int, phoneNumber :String) : Result<ImmutableProfile> {
+                  age : Int, gender : Gender, sciper : Int?, phoneNumber :String, role : Role) : Result<ImmutableProfile> {
             return when{
                 lastName.length > MAX_LAST_NAME_SIZE ->  Failure("last name is more than $MAX_LAST_NAME_SIZE characters")
                 firstName.length > MAX_FIRST_NAME_SIZE -> Failure("first name is more than $MAX_FIRST_NAME_SIZE characters")
                 age > MAX_AGE -> Failure("age is more than $MAX_AGE")
                 age <  MIN_AGE -> Failure("age is less than $MIN_AGE")
-                sciper < 0 -> Failure("sciper can't be a negative value")
+                (sciper!! < 0) -> Failure("sciper can't be a negative value")
                 else -> Success(ImmutableProfile(lastName,  firstName,
-                    age,  gender,  sciper,  phoneNumber))
+                    age,  gender,  sciper,  phoneNumber,role))
 
             }
         }
@@ -50,13 +51,14 @@ class ImmutableProfile private constructor(val lastName : String, val firstName 
      * @param gender : gender on the profile
      * @param sciper : sciper on the profile
      * @param phoneNumber : phone number on the profile
+     * @param role : role on the profile
      * @return Success(profile) or failure(String explanation)
      */
     fun buildCopy(lastName: String = this.lastName, firstName : String = this.firstName,
-                  age : Int = this.age, gender : Gender = this.gender, sciper : Int = this.sciper,
-                  phoneNumber :String = this.phoneNumber) : Result<ImmutableProfile>{
+                  age : Int = this.age, gender : Gender = this.gender, sciper : Int? = this.sciper,
+                  phoneNumber :String = this.phoneNumber, role : Role = this.role) : Result<ImmutableProfile>{
         return ImmutableProfile.build(lastName,  firstName,
-            age,  gender,  sciper,  phoneNumber)
+            age,  gender,  sciper,  phoneNumber,role)
 
     }
 
--- app/src/main/java/com/sdp13epfl2021/projmag/model/ProfileClass.kt
+++
@@ -1,5 +0,0 @@
-package com.sdp13epfl2021.projmag.model
-
-abstract class ProfileClass(val lastName : String, val firstName : String, val age : Int, val gender : Gender, val phoneNumber : String) {
-
-}
