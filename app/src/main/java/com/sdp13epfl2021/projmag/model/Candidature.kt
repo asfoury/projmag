46a1be97523da1dea7ca7d8aae18b3b03b47bee5
arnauds5 <arnauds5@users.noreply.github.com>
2021-04-20 13:10:09 +0200

    Add Parcelize & a State to the Candidature data class

--- app/src/main/java/com/sdp13epfl2021/projmag/model/Candidature.kt
+++ app/src/main/java/com/sdp13epfl2021/projmag/model/Candidature.kt
@@ -1,6 +1,19 @@
 package com.sdp13epfl2021.projmag.model
 
+import android.os.Parcelable
 import com.sdp13epfl2021.projmag.curriculumvitae.CurriculumVitae
 import com.sdp13epfl2021.projmag.database.ProjectId
+import kotlinx.parcelize.Parcelize
 
-data class Candidature(val projectId: ProjectId, val userID: String, val profile: ImmutableProfile, val cv: CurriculumVitae)
+@Parcelize
+data class Candidature(
+    val projectId: ProjectId,
+    val userID: String,
+    val profile: ImmutableProfile,
+    val cv: CurriculumVitae,
+    val state: State
+) : Parcelable {
+
+    enum class State { Waiting, Accepted, Rejected }
+}
+
