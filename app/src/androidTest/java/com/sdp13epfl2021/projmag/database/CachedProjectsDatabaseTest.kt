0f2695684d7736acae6572af89a2a05453dbab7e
kaourintin <kaourintin.tamine.personal@gmail.com>
2021-04-21 21:02:44 +0200

    fixed tests in cached project not having valid tags

--- app/src/androidTest/java/com/sdp13epfl2021/projmag/database/CachedProjectsDatabaseTest.kt
+++ app/src/androidTest/java/com/sdp13epfl2021/projmag/database/CachedProjectsDatabaseTest.kt
@@ -1,6 +1,8 @@
 package com.sdp13epfl2021.projmag.database
 
 import com.sdp13epfl2021.projmag.model.ImmutableProject
+import com.sdp13epfl2021.projmag.model.SectionBaseManager
+import com.sdp13epfl2021.projmag.model.TagsBaseManager
 import junit.framework.TestCase
 import org.junit.Assert.*
 import org.junit.Test
@@ -8,6 +10,10 @@ import java.util.concurrent.atomic.AtomicInteger
 
 
 class CachedProjectsDatabaseTest {
+    private val tagManager = TagsBaseManager()
+    private val sectionManager = SectionBaseManager()
+    private val tagList : List<String> = tagManager.tagsListToStringList(tagManager.getAllTags())
+    private val sectionList : List<String> = sectionManager.sectionList()
     private val p1 = ImmutableProject(
         "12345",
         "What fraction of Google searches are answered by Wikipedia?",
@@ -18,9 +24,11 @@ class CachedProjectsDatabaseTest {
         listOf<String>(),
         false,
         true,
-        listOf("data analysis","large datasets","database","systems","database","systems"),
+        listOf(tagList[0], tagList[3]),
         false,
         "Description of project1",
+        listOf(),
+        listOf(sectionList[0])
     )
     private val p2 = ImmutableProject(
         "11111",
@@ -32,9 +40,11 @@ class CachedProjectsDatabaseTest {
         listOf<String>(),
         false,
         true,
-        listOf("Computer Vision","ML"),
+        listOf(tagList[3], tagList[0]),
         false,
         "Description of project2",
+        listOf(),
+        listOf(sectionList[3])
     )
     private val p3 = ImmutableProject(
         "00000",
@@ -46,9 +56,11 @@ class CachedProjectsDatabaseTest {
         listOf<String>(),
         false,
         true,
-        listOf("Low Level","Networking","Driver"),
+        listOf(tagList[2]),
         false,
         "Description of project5",
+        listOf(),
+        listOf(sectionList[2])
     )
 
 
@@ -181,7 +193,7 @@ class CachedProjectsDatabaseTest {
         val cachedDB = CachedProjectsDatabase(fakeDB)
         var result: List<ImmutableProject>? = null
 
-        cachedDB.getProjectsFromTags(listOf("ml", "Driver"), { projects ->
+        cachedDB.getProjectsFromTags(listOf(tagList[0], tagList[3]), { projects ->
             result = projects
         }, { e ->
             assertNull(e)
@@ -189,8 +201,8 @@ class CachedProjectsDatabaseTest {
 
         while (result == null);
         assertEquals(2, result!!.size)
+        assertTrue(result!!.contains(p1))
         assertTrue(result!!.contains(p2))
-        assertTrue(result!!.contains(p3))
     }
 
     @Test(timeout = 1000)
