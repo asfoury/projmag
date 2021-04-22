4fe76eb261addf1330c4aa5d59a03e813f978c72
Mohamed Elasfoury <mohamed.elasfoury@epfl.ch>
2021-04-23 01:07:04 +0200

    Started working on mockito tests

---
+++ app/src/test/java/com/sdp13epfl2021/projmag/database/UserProfileDatabaseTest.kt
@@ -0,0 +1,32 @@
+package com.sdp13epfl2021.projmag.database
+
+
+import android.util.Log
+import com.sdp13epfl2021.projmag.model.*
+import org.junit.Before
+import org.mockito.Mockito
+
+class UserProfileDatabaseTest {
+//    val mockProfileDb = Mockito.mock(UserProfileDatabase::class.java)
+//    val exampleUserProfile = when (val prof = ImmutableProfile.build(
+//        "Last Name",
+//        "First Name",
+//        22,
+//        Gender.MALE,
+//        289982,
+//        "01001001",
+//        Role.STUDENT
+//    )) {
+//        is Success -> prof.value
+//        is Failure -> null
+//    }
+//
+//    @Suppress("UNCHECKED_CAST")
+//    @Before
+//    fun setupMocks() {
+//       Mockito.`when`(mockProfileDb.getProfile())
+//
+//        Mockito.`when`(mockProfileDb.uploadProfile(exampleUserProfile!!, {},{}))
+//    }
+
+}
