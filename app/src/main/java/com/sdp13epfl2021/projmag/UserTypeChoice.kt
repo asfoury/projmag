258d7cf1975d55f99b8cb54a827b2105897306f5
Giovanni Michel Igowa <giovanni.igowa@epfl.ch>
2021-04-28 16:15:27 +0200

    rename activity and change variable

--- app/src/main/java/com/sdp13epfl2021/projmag/UserTypeChoice.kt
+++ app/src/main/java/com/sdp13epfl2021/projmag/UserTypeChoice.kt
@@ -2,29 +2,26 @@ package com.sdp13epfl2021.projmag
 
 import android.content.Intent
 import android.os.Bundle
-import android.util.Log
 import android.widget.RadioButton
 import androidx.appcompat.app.AppCompatActivity
 import com.sdp13epfl2021.projmag.activities.ProfilePageActivity
 
 class UserTypeChoice : AppCompatActivity() {
-    lateinit var ProfButton: RadioButton
-    lateinit var StuButton: RadioButton
 
     companion object{
         var isProfessor: Boolean = false
     }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_user_type2)
-        ProfButton = findViewById(R.id.radioProfessorType)
-        StuButton = findViewById(R.id.radioStudentType)
+        setContentView(R.layout.activity_user_type_choice)
+        val professorButton: RadioButton = findViewById(R.id.radioProfessorType)
+        val studentButton: RadioButton= findViewById(R.id.radioStudentType)
         val intent = Intent(this, ProfilePageActivity::class.java)
-        ProfButton.setOnClickListener{
+        professorButton.setOnClickListener{
             isProfessor = true
             startActivity(intent)
         }
-        StuButton.setOnClickListener{
+        studentButton.setOnClickListener{
             isProfessor = false
             startActivity(intent)
         }
