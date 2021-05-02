7d224571f2cd5fcb70974534691da8117d4be221
asfoury <mohamed.elasfoury@epfl.ch>
2021-05-02 18:53:38 +0200

    Improve coverage

--- app/src/androidTest/java/com/sdp13epfl2021/projmag/ProfilePageActivityTest.kt
+++ app/src/androidTest/java/com/sdp13epfl2021/projmag/ProfilePageActivityTest.kt
@@ -18,12 +18,20 @@ class ProfilePageActivityTest {
         ActivityScenarioRule(ProfilePageActivity::class.java)
 
     @Test
-    fun test() {
+    fun testChangingValues() {
         onView(withId(R.id.profile_lastname)).perform(scrollTo(),clearText(), typeText("lastName"))
         onView(withId(R.id.profile_firstname)).perform(scrollTo(),clearText(), typeText("firstName"))
         onView(withId(R.id.email_profile)).perform(scrollTo(),clearText(), typeText("aaa@aaa"))
         onView(withId(R.id.profile_age)).perform(scrollTo(),clearText(), typeText("21"))
         onView(withId(R.id.profile_genre)).perform(scrollTo(),clearText(), typeText("Male"))
         onView(withId(R.id.profile_phone_number)).perform(scrollTo(),clearText(), typeText("0101010100"))
+    }
+
+
+    @Test
+    fun changeProfileValuesAndSumbmit() {
+        onView(withId(R.id.email_profile)).perform(scrollTo(),clearText(), typeText("aaa@aaa"))
+        onView(withId(R.id.buttonSubChangeProfil)).perform(scrollTo(), click())
+
     }
 }
