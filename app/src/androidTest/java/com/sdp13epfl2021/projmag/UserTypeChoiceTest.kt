e0fd64fa2b85b53fa07ccdf83c1d234a70d12e31
Giovanni Michel Igowa <giovanni.igowa@epfl.ch>
2021-05-04 17:41:20 +0200

    remove expresso test

--- app/src/androidTest/java/com/sdp13epfl2021/projmag/UserTypeChoiceTest.kt
+++ app/src/androidTest/java/com/sdp13epfl2021/projmag/UserTypeChoiceTest.kt
@@ -21,118 +21,29 @@ import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
 
-@LargeTest
-@RunWith(AndroidJUnit4::class)
+
 class UserTypeChoiceTest {
 
-    @Rule
-    @JvmField
+    @get:Rule
     var mActivityTestRule = ActivityTestRule(UserTypeChoice::class.java)
 
     @Test
-    fun userTypeChoiceTest() {
-        val materialRadioButton = onView(
-            allOf(
-                withId(R.id.radioProfessorType), withText("Professor"),
-                childAtPosition(
-                    childAtPosition(
-                        withClassName(`is`("androidx.constraintlayout.widget.ConstraintLayout")),
-                        0
-                    ),
-                    0
-                ),
-                isDisplayed()
-            )
-        )
-        materialRadioButton.perform(click())
+    fun userProf() {
+        onView(withId(R.id.radioProfessorType)).perform(click())
         assertTrue(UserTypeChoice.isProfessor)
+    }
 
-
-        val appCompatImageButton = onView(
-            allOf(
-                withContentDescription("Navigate up"),
-                childAtPosition(
-                    allOf(
-                        withId(R.id.action_bar),
-                        childAtPosition(
-                            withId(R.id.action_bar_container),
-                            0
-                        )
-                    ),
-                    1
-                ),
-                isDisplayed()
-            )
-        )
-        appCompatImageButton.perform(click())
-
-
-        val materialRadioButton2 = onView(
-            allOf(
-                withId(R.id.radioStudentType), withText("Student"),
-                childAtPosition(
-                    childAtPosition(
-                        withClassName(`is`("androidx.constraintlayout.widget.ConstraintLayout")),
-                        0
-                    ),
-                    1
-                ),
-                isDisplayed()
-            )
-        )
-        materialRadioButton2.perform(click())
+    @Test
+    fun userStudent() {
+        onView(withId(R.id.radioStudentType)).perform(click())
         assertFalse(UserTypeChoice.isProfessor)
+    }
 
-        val appCompatImageButton2 = onView(
-            allOf(
-                withContentDescription("Navigate up"),
-                childAtPosition(
-                    allOf(
-                        withId(R.id.action_bar),
-                        childAtPosition(
-                            withId(R.id.action_bar_container),
-                            0
-                        )
-                    ),
-                    1
-                ),
-                isDisplayed()
-            )
-        )
-        appCompatImageButton2.perform(click())
-
-        val materialRadioButton3 = onView(
-            allOf(
-                withId(R.id.radioPHDType), withText("PHD"),
-                childAtPosition(
-                    childAtPosition(
-                        withClassName(`is`("androidx.constraintlayout.widget.ConstraintLayout")),
-                        0
-                    ),
-                    2
-                ),
-                isDisplayed()
-            )
-        )
-        materialRadioButton3.perform(click())
+    @Test
+    fun userPHD() {
+        onView(withId(R.id.radioPHDType)).perform(click())
         assertTrue(UserTypeChoice.isProfessor)
     }
 
-    private fun childAtPosition(
-        parentMatcher: Matcher<View>, position: Int
-    ): Matcher<View> {
 
-        return object : TypeSafeMatcher<View>() {
-            override fun describeTo(description: Description) {
-                description.appendText("Child at position $position in parent ")
-                parentMatcher.describeTo(description)
-            }
-
-            public override fun matchesSafely(view: View): Boolean {
-                val parent = view.parent
-                return parent is ViewGroup && parentMatcher.matches(parent)
-                        && view == parent.getChildAt(position)
-            }
-        }
-    }
 }
