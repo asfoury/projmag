7a6a8a5df813f9e47a596ae99e7a845f9be6fd2e
asfoury <mohamed.elasfoury@epfl.ch>
2021-05-09 20:00:58 +0200

    Fixes after first code review

--- app/src/main/java/com/sdp13epfl2021/projmag/database/interfaces/UserdataDatabase.kt
+++ app/src/main/java/com/sdp13epfl2021/projmag/database/interfaces/UserdataDatabase.kt
@@ -155,7 +155,7 @@ interface UserdataDatabase {
     )
 
     /**
-     * Uploads the user profile to firebase
+     * Uploads the user profile to a remote database
      * @param profile the user profile to upload
      * @param onSuccess the closure that's when a profile is uploaded successfully
      * @param onFailure the closure that's called if the upload fails
@@ -167,12 +167,12 @@ interface UserdataDatabase {
     )
 
     /**
-     * Gets the user profile from firebase if it exists
+     * Gets the user profile from the remote database if it exists
      * @param onSuccess the closure that's when a profile is downloaded successfully with the fetched profile passed to it
      * @param onFailure the closure that's called if the download fails
      */
     fun getProfile(
         onSuccess: (profile: ImmutableProfile?) -> Unit,
-        onFailure: (Exception) -> kotlin.Unit
+        onFailure: (Exception) -> Unit
     )
 }
