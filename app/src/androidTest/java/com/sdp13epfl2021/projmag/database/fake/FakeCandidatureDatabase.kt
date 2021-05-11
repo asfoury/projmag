06ef985e7c3c45b458d3034f93dfb9f6b952665a
lpatel99 <luke.patel@hotmail.co.uk>
2021-05-11 21:29:19 +0200

    implemented missing method in fakecandidaturedatabase

--- app/src/androidTest/java/com/sdp13epfl2021/projmag/database/fake/FakeCandidatureDatabase.kt
+++ app/src/androidTest/java/com/sdp13epfl2021/projmag/database/fake/FakeCandidatureDatabase.kt
@@ -11,11 +11,11 @@ class FakeCandidatureDatabase(
 ) : CandidatureDatabase {
 
     override fun getListOfCandidatures(
-        projectID: ProjectId,
+        projectId: ProjectId,
         onSuccess: (List<Candidature>) -> Unit,
         onFailure: (Exception) -> Unit
     ) {
-        onSuccess(candidatures[projectID]?.values?.toList() ?: emptyList())
+        onSuccess(candidatures[projectId]?.values?.toList() ?: emptyList())
     }
 
     override fun pushCandidature(
@@ -24,20 +24,38 @@ class FakeCandidatureDatabase(
         onSuccess: () -> Unit,
         onFailure: (Exception) -> Unit
     ) {
-        val projectID = candidature.projectId
-        val userID = candidature.userId
-        val newMap1 = candidaturesState[projectID] ?: emptyMap()
-        val newMap2 = candidatures[projectID] ?: emptyMap()
-        candidaturesState[projectID] = newMap1 + (userID to newState)
-        candidatures[projectID] = newMap2 + (userID to candidature)
-        onChanges[projectID]?.let { it.forEach { it(projectID, candidatures[projectID]?.values?.toList() ?: emptyList()) } }
+        val projectId = candidature.projectId
+        val userId = candidature.userId
+        val newMap1 = candidaturesState[projectId] ?: emptyMap()
+        val newMap2 = candidatures[projectId] ?: emptyMap()
+        candidaturesState[projectId] = newMap1 + (userId to newState)
+        candidatures[projectId] = newMap2 + (userId to candidature)
+        onChanges[projectId]?.let { it.forEach { it(projectId, candidatures[projectId]?.values?.toList() ?: emptyList()) } }
+        onSuccess()
+    }
+
+    override fun removeCandidature(
+        projectId: ProjectId,
+        userId: String,
+        onSuccess: () -> Unit,
+        onFailure: (Exception) -> Unit
+    ) {
+        val temp = candidatures[projectId] ?: emptyMap()
+        val newMap = temp.toMutableMap()
+        newMap.remove(userId)
+        candidatures[projectId] = newMap.toMap()
+        onChanges[projectId]?.let {
+            it.forEach {
+                it(projectId, candidatures[projectId]?.values?.toList() ?: emptyList())
+            }
+        }
         onSuccess()
     }
 
     override fun addListener(
-        projectID: ProjectId,
+        projectId: ProjectId,
         onChange: (ProjectId, List<Candidature>) -> Unit
     ) {
-        onChanges[projectID]?.let { it.add(onChange) } ?: run { onChanges[projectID] = mutableListOf(onChange)}
+        onChanges[projectId]?.let { it.add(onChange) } ?: run { onChanges[projectId] = mutableListOf(onChange)}
     }
 }
