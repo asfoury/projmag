c3d36dfedf6f7b46e667cb948002092bb111b08e
Paul Coral <paul.coral@epfl.ch>
2021-05-13 16:38:08 +0200

    Use cached database

--- app/src/androidTest/java/com/sdp13epfl2021/projmag/PreferencesActivityTests.kt
+++ app/src/androidTest/java/com/sdp13epfl2021/projmag/PreferencesActivityTests.kt
@@ -8,7 +8,7 @@ import androidx.test.ext.junit.rules.ActivityScenarioRule
 import com.sdp13epfl2021.projmag.JavaToKotlinHelperAndroidTest.anyObject
 import com.sdp13epfl2021.projmag.activities.PreferencesActivity
 import com.sdp13epfl2021.projmag.database.interfaces.UserdataDatabase
-import com.sdp13epfl2021.projmag.database.interfaces.UserdataDatabaseModule
+import com.sdp13epfl2021.projmag.database.interfaces.DataBaseModules
 import com.sdp13epfl2021.projmag.model.ProjectFilter
 import dagger.hilt.android.testing.BindValue
 import dagger.hilt.android.testing.HiltAndroidRule
@@ -20,7 +20,7 @@ import org.junit.Test
 import org.junit.rules.RuleChain
 import org.mockito.Mockito
 
-@UninstallModules(UserdataDatabaseModule::class)
+@UninstallModules(DataBaseModules::class)
 @HiltAndroidTest
 class PreferencesActivityTests {
 
--- app/src/main/java/com/sdp13epfl2021/projmag/database/interfaces/DatabaseModules.kt
+++ app/src/main/java/com/sdp13epfl2021/projmag/database/interfaces/DatabaseModules.kt
@@ -1,35 +1,48 @@
 package com.sdp13epfl2021.projmag.database.interfaces
 
+import android.content.Context
 import com.google.firebase.auth.FirebaseAuth
-import com.google.firebase.auth.ktx.auth
 import com.google.firebase.firestore.FirebaseFirestore
-import com.google.firebase.firestore.ktx.firestore
-import com.google.firebase.ktx.Firebase
+import com.sdp13epfl2021.projmag.database.impl.cache.OfflineCachedUserdataDatabase
 import com.sdp13epfl2021.projmag.database.impl.firebase.FirebaseUserdataDatabase
-import dagger.Binds
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
 import dagger.hilt.components.SingletonComponent
+import java.io.File
+import javax.inject.Named
 import javax.inject.Singleton
 
 @Module
 @InstallIn(SingletonComponent::class)
-abstract class UserdataDatabaseModule {
-    @Binds
-    @Singleton
-    abstract fun bindUserdataDatabase(
-        userDB: FirebaseUserdataDatabase
-    ): UserdataDatabase
-}
+object DataBaseModules {
 
-
-@Module
-@InstallIn(SingletonComponent::class)
-object FirebaseModule {
+    /**
+     * this Provides an instance of UserDataDatabase
+     */
     @Provides
+    @Singleton
+    fun provideUserdataDatabase(
+        ff: FirebaseFirestore,
+        auth: FirebaseAuth,
+        @Named("fileCacheUserDB") fc: File
+    ): UserdataDatabase = OfflineCachedUserdataDatabase(
+        FirebaseUserdataDatabase(ff, auth),
+        auth.uid ?: "",
+        fc
+    )
+
+    @Provides
+    @Singleton
     fun provideFirebaseFirestore(): FirebaseFirestore = FirebaseFirestore.getInstance()
 
     @Provides
+    @Singleton
     fun provideFirebaseAuth(): FirebaseAuth = FirebaseAuth.getInstance()
+
+    @Provides
+    @Named("fileCacheUserDB")
+    fun fileForCache(@ApplicationContext applicationContext: Context): File =
+        File(applicationContext.filesDir, "users")
 }
