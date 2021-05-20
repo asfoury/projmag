0080eeaa924ad06ade97e9289c86f5632295de30
Paul Coral <paul.coral@epfl.ch>
2021-05-20 15:12:36 +0200

    Fix UserTypeChoiceTest.kt

--- app/src/androidTest/java/com/sdp13epfl2021/projmag/UserTypeChoiceTest.kt
+++ app/src/androidTest/java/com/sdp13epfl2021/projmag/UserTypeChoiceTest.kt
@@ -5,23 +5,35 @@ import androidx.test.espresso.Espresso.onView
 import androidx.test.espresso.action.ViewActions.click
 import androidx.test.espresso.matcher.ViewMatchers.withId
 import androidx.test.ext.junit.rules.ActivityScenarioRule
-import androidx.test.rule.ActivityTestRule
-import com.sdp13epfl2021.projmag.activities.CVCreationActivity
+import com.sdp13epfl2021.projmag.JavaToKotlinHelperAndroidTest.anyObject
 import com.sdp13epfl2021.projmag.activities.UserTypeChoice
+import com.sdp13epfl2021.projmag.database.di.UserdataDatabaseModule
+import com.sdp13epfl2021.projmag.database.interfaces.UserdataDatabase
+import dagger.hilt.android.testing.BindValue
 import dagger.hilt.android.testing.HiltAndroidRule
 import dagger.hilt.android.testing.HiltAndroidTest
+import dagger.hilt.android.testing.UninstallModules
 import junit.framework.Assert.assertFalse
 import junit.framework.Assert.assertTrue
 import org.junit.Rule
 import org.junit.Test
 import org.junit.rules.RuleChain
+import org.mockito.Mockito
 
+@UninstallModules(UserdataDatabaseModule::class)
 @HiltAndroidTest
 class UserTypeChoiceTest {
+    private val activityScenario = ActivityScenarioRule(UserTypeChoice::class.java)
 
     @get:Rule
     var testRule: RuleChain = RuleChain.outerRule(HiltAndroidRule(this))
-        .around(ActivityScenarioRule(UserTypeChoice::class.java))
+        .around(activityScenario)
+
+    @BindValue
+    val mUserDB: UserdataDatabase = Mockito.mock(UserdataDatabase::class.java).also {
+        // this is to avoid using the real DB when starting the next activity
+        Mockito.`when`(it.getProfile(anyObject(), anyObject(), anyObject())).then {}
+    }
 
     @Test
     fun userProf() {
