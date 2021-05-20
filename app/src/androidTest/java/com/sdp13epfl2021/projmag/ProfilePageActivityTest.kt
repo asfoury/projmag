19050b336abb36fc960b51359867ceeca79a7b63
Paul Coral <paul.coral@epfl.ch>
2021-05-21 00:40:39 +0200

    Fix crashing ProfilePageActivityTest.kt

--- app/src/androidTest/java/com/sdp13epfl2021/projmag/ProfilePageActivityTest.kt
+++ app/src/androidTest/java/com/sdp13epfl2021/projmag/ProfilePageActivityTest.kt
@@ -9,13 +9,12 @@ import androidx.test.espresso.matcher.ViewMatchers.withText
 import androidx.test.ext.junit.rules.ActivityScenarioRule
 import com.sdp13epfl2021.projmag.JavaToKotlinHelperAndroidTest.anyObject
 import com.sdp13epfl2021.projmag.activities.ProfilePageActivity
+import com.sdp13epfl2021.projmag.database.di.ProjectDatabaseModule
 import com.sdp13epfl2021.projmag.database.di.UserIdModule
 import com.sdp13epfl2021.projmag.database.di.UserdataDatabaseModule
+import com.sdp13epfl2021.projmag.database.interfaces.ProjectDatabase
 import com.sdp13epfl2021.projmag.database.interfaces.UserdataDatabase
-import com.sdp13epfl2021.projmag.model.Gender
-import com.sdp13epfl2021.projmag.model.ImmutableProfile
-import com.sdp13epfl2021.projmag.model.Role
-import com.sdp13epfl2021.projmag.model.Success
+import com.sdp13epfl2021.projmag.model.*
 import dagger.hilt.android.testing.BindValue
 import dagger.hilt.android.testing.HiltAndroidRule
 import dagger.hilt.android.testing.HiltAndroidTest
@@ -27,73 +26,86 @@ import org.junit.rules.RuleChain
 import org.mockito.Mockito
 import javax.inject.Named
 
-@UninstallModules(UserdataDatabaseModule::class, UserIdModule::class)
+@UninstallModules(ProjectDatabaseModule::class, UserdataDatabaseModule::class, UserIdModule::class)
 @HiltAndroidTest
 class ProfilePageActivityTest {
 
     @get:Rule
     var testRule: RuleChain = RuleChain.outerRule(HiltAndroidRule(this))
-        .around(ActivityScenarioRule(ProfilePageActivity::class.java))
+            .around(ActivityScenarioRule(ProfilePageActivity::class.java))
 
     @BindValue
     @Named("currentUserId")
     val userID: String = "uid"
 
     val dummyProfile = (ImmutableProfile.build(
-        lastName = "Foo",
-        firstName = "Bar",
-        42,
-        Gender.MALE,
-        0,
-        "00000",
-        Role.STUDENT // only student role for now
+            lastName = "Foo",
+            firstName = "Bar",
+            42,
+            Gender.MALE,
+            0,
+            "00000",
+            Role.STUDENT // only student role for now
     ) as Success<ImmutableProfile>).value
 
     val otherDummyProfile = (ImmutableProfile.build(
-        lastName = "first",
-        firstName = "last",
-        22,
-        Gender.FEMALE,
-        0,
-        "0101010100",
-        Role.STUDENT // only student role for now
+            lastName = "first",
+            firstName = "last",
+            22,
+            Gender.FEMALE,
+            0,
+            "0101010100",
+            Role.STUDENT // only student role for now
     ) as Success<ImmutableProfile>).value
 
     @BindValue
     val userDB: UserdataDatabase =
-        Mockito.mock(UserdataDatabase::class.java).apply {
-            Mockito.`when`(this.getProfile(anyObject(), anyObject(), anyObject()))
-                .then {
-                    assertEquals(userID, it.arguments[0])
-                    @Suppress("UNCHECKED_CAST")
-                    val success = it.arguments[1] as Function1<ImmutableProfile, Unit>
-                    success(dummyProfile)
-                }
+            Mockito.mock(UserdataDatabase::class.java).apply {
+                Mockito.`when`(this.getProfile(anyObject(), anyObject(), anyObject()))
+                        .then {
+                            assertEquals(userID, it.arguments[0])
+                            @Suppress("UNCHECKED_CAST")
+                            val success = it.arguments[1] as Function1<ImmutableProfile, Unit>
+                            success(dummyProfile)
+                        }
+            }
+
+    @BindValue
+    val projectDB: ProjectDatabase = Mockito.mock(ProjectDatabase::class.java).also { db ->
+        // needed because Intent going to ProjectListActivty
+        Mockito.`when`(db.getAllProjects(anyObject(), anyObject())).then {
+            @Suppress("UNCHECKED_CAST")
+            val onSuccess = it.arguments[0] as Function1<List<ImmutableProject>, Unit>
+            onSuccess(listOf())
         }
+
+        Mockito.`when`(db.addProjectsChangeListener(anyObject())).then {
+            /* Does nothing for now */
+        }
+    }
 
     @Test
     fun loadValuesAreDisplayedCorrectly() {
         Thread.sleep(1000) // wait UI to UPDATE
         onView(withId(R.id.profile_lastname)).perform(scrollTo())
-            .check(matches(withText(dummyProfile.lastName)))
+                .check(matches(withText(dummyProfile.lastName)))
 
         onView(withId(R.id.profile_firstname)).perform(scrollTo())
-            .check(matches(withText(dummyProfile.firstName)))
+                .check(matches(withText(dummyProfile.firstName)))
 
         onView(withId(R.id.profile_age)).perform(scrollTo())
-            .check(matches(withText(dummyProfile.age.toString())))
+                .check(matches(withText(dummyProfile.age.toString())))
 
         onView(withId(R.id.profile_genre)).perform(scrollTo())
-            .check(matches(withText(dummyProfile.gender.toString())))
+                .check(matches(withText(dummyProfile.gender.toString())))
         onView(withId(R.id.profile_phone_number)).perform(scrollTo())
-            .check(matches(withText(dummyProfile.phoneNumber)))
+                .check(matches(withText(dummyProfile.phoneNumber)))
         onView(withId(R.id.profile_sciper))
-            .check(matches(withText(dummyProfile.sciper.toString())))
+                .check(matches(withText(dummyProfile.sciper.toString())))
     }
 
     @Test
     fun profileIsSubmittedCorrectly() {
-        Thread.sleep(1000) // wait UI to UPDATE
 
         Mockito.`when`(userDB.uploadProfile(anyObject(), anyObject(), anyObject())).then {
             val profile = it.arguments[0] as ImmutableProfile
@@ -105,21 +117,21 @@ class ProfilePageActivityTest {
                 replaceText(otherDummyProfile.lastName)
         )
         onView(withId(R.id.profile_firstname)).perform(
-            scrollTo(),
-            replaceText(otherDummyProfile.firstName)
+                scrollTo(),
+                replaceText(otherDummyProfile.firstName)
         )
         onView(withId(R.id.profile_age)).perform(
-            scrollTo(),
-            replaceText(otherDummyProfile.age.toString())
+                scrollTo(),
+                replaceText(otherDummyProfile.age.toString())
         )
         onView(withId(R.id.profile_genre)).perform(
-            scrollTo(),
-            replaceText(otherDummyProfile.gender.name)
+                scrollTo(),
+                replaceText(otherDummyProfile.gender.name)
         )
         onView(withId(R.id.profile_phone_number)).perform(
-            scrollTo(),
-            replaceText(otherDummyProfile.phoneNumber),
-            closeSoftKeyboard()
+                scrollTo(),
+                replaceText(otherDummyProfile.phoneNumber),
+                closeSoftKeyboard()
         )
 
         onView(withId(R.id.button_sub_change_profile)).perform(click())
