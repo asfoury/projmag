3a2ab392871ad4a1917a4654a7e67caaa7ac768b
Paul Coral <paul.coral@epfl.ch>
2021-05-20 11:46:05 +0200

    Fix typo in UserdataDatabaseModule.kt

--- app/src/main/java/com/sdp13epfl2021/projmag/database/di/UserdataDatabaseModule.kt
+++ app/src/main/java/com/sdp13epfl2021/projmag/database/di/UserdataDatabaseModule.kt
@@ -1,11 +1,8 @@
 package com.sdp13epfl2021.projmag.database.di
 
-import com.google.firebase.auth.FirebaseAuth
-import com.google.firebase.firestore.FirebaseFirestore
 import com.sdp13epfl2021.projmag.database.impl.cache.OfflineCachedUserdataDatabase
 import com.sdp13epfl2021.projmag.database.impl.firebase.FirebaseUserdataDatabase
 import com.sdp13epfl2021.projmag.database.interfaces.UserdataDatabase
-import dagger.Binds
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
@@ -20,9 +17,9 @@ object UserdataDatabaseModule {
     @Provides
     @Singleton
     fun bindUserdataDatabase(
-        userDB: FirebaseUserdataDatabase,
-        @Named("userId") uid: String,
-        @Named("userRootDir") dir: File
+            userDB: FirebaseUserdataDatabase,
+            @Named("userID") uid: String,
+            @Named("userRootDir") dir: File
     ): UserdataDatabase =
         OfflineCachedUserdataDatabase(
             db = userDB,
