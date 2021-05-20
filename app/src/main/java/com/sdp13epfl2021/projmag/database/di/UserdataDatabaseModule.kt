fd46f264efdcecd7600a3cec0a9959ca43b48cf8
Paul Coral <paul.coral@epfl.ch>
2021-05-20 12:29:25 +0200

    Update and fix after merging with main

--- app/src/main/java/com/sdp13epfl2021/projmag/database/di/UserdataDatabaseModule.kt
+++ app/src/main/java/com/sdp13epfl2021/projmag/database/di/UserdataDatabaseModule.kt
@@ -17,9 +17,9 @@ object UserdataDatabaseModule {
     @Provides
     @Singleton
     fun bindUserdataDatabase(
-            userDB: FirebaseUserdataDatabase,
-            @Named("userID") uid: String,
-            @Named("userRootDir") dir: File
+        userDB: FirebaseUserdataDatabase,
+        @Named("currentUserId") uid: String,
+        @Named("fileCacheUserDB") dir: File
     ): UserdataDatabase =
         OfflineCachedUserdataDatabase(
             db = userDB,
