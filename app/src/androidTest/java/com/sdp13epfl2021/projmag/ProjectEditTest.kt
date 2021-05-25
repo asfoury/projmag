3aea22dc3421ef0025fd64bb06a03ad3af8663ca
Arnaud S5 <arnauds5@users.noreply.github.com>
2021-05-25 09:19:08 +0200

    Update app/src/androidTest/java/com/sdp13epfl2021/projmag/ProjectEditTest.kt
    Fix imports.

--- app/src/androidTest/java/com/sdp13epfl2021/projmag/ProjectEditTest.kt
+++ app/src/androidTest/java/com/sdp13epfl2021/projmag/ProjectEditTest.kt
@@ -8,6 +8,7 @@ import androidx.test.espresso.action.ViewActions.*
 import androidx.test.espresso.matcher.ViewMatchers.withId
 import androidx.test.ext.junit.rules.ActivityScenarioRule
 import androidx.test.ext.junit.runners.AndroidJUnit4
+import com.sdp13epfl2021.projmag.activities.ProjectCreationActivity
 import com.sdp13epfl2021.projmag.model.ImmutableProject
 import org.junit.Rule
 import org.junit.Test
