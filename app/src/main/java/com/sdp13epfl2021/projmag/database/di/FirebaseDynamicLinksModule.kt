cf9491a7bb47791bbdbd44c8c2d15f70b06b4644
Paul Coral <paul.coral@epfl.ch>
2021-05-25 20:34:05 +0200

    Fix FirebaseDynamicLinksModule.kt

--- app/src/main/java/com/sdp13epfl2021/projmag/database/di/FirebaseDynamicLinksModule.kt
+++ app/src/main/java/com/sdp13epfl2021/projmag/database/di/FirebaseDynamicLinksModule.kt
@@ -10,7 +10,7 @@ import javax.inject.Singleton
 
 @Module
 @InstallIn(SingletonComponent::class)
-class FirebaseDynamicLinksModule {
+object FirebaseDynamicLinksModule {
     @Provides
     @Singleton
     fun provideFirebaseDynamicLinks(): FirebaseDynamicLinks = FirebaseDynamicLinks.getInstance()
