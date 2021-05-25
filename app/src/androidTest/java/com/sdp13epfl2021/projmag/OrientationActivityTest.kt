608cbc7edc47ba4ef2db52dbb3e02b4410480541
Paul Coral <paul.coral@epfl.ch>
2021-05-25 19:08:45 +0200

    Some fixes in Sign-In activity

--- app/src/androidTest/java/com/sdp13epfl2021/projmag/OrientationActivityTest.kt
+++ app/src/androidTest/java/com/sdp13epfl2021/projmag/OrientationActivityTest.kt
@@ -1,11 +1,14 @@
 package com.sdp13epfl2021.projmag
 
+import androidx.test.ext.junit.runners.AndroidJUnit4
 import androidx.test.rule.ActivityTestRule
 import com.sdp13epfl2021.projmag.activities.OrientationActivity
 import junit.framework.Assert.assertEquals
 import org.junit.Rule
 import org.junit.Test
+import org.junit.runner.RunWith
 
+@RunWith(AndroidJUnit4::class)
 class OrientationActivityTest {
     @get:Rule
     var mActivityTestRule = ActivityTestRule(OrientationActivity::class.java)
--- app/src/androidTest/java/com/sdp13epfl2021/projmag/SignInActivityTest.kt
+++ app/src/androidTest/java/com/sdp13epfl2021/projmag/SignInActivityTest.kt
@@ -1,21 +1,28 @@
 package com.sdp13epfl2021.projmag
 
 
+import android.provider.Settings
 import androidx.test.espresso.Espresso.onView
 import androidx.test.espresso.action.ViewActions.click
+import androidx.test.espresso.intent.Intents
+import androidx.test.espresso.intent.matcher.IntentMatchers
 import androidx.test.espresso.matcher.ViewMatchers.withId
 import androidx.test.ext.junit.rules.ActivityScenarioRule
-import androidx.test.runner.AndroidJUnit4
 import com.sdp13epfl2021.projmag.activities.SignInActivity
+import dagger.hilt.android.testing.HiltAndroidRule
+import dagger.hilt.android.testing.HiltAndroidTest
 import org.junit.Rule
 import org.junit.Test
-import org.junit.runner.RunWith
+import org.junit.rules.RuleChain
 
-@RunWith(AndroidJUnit4::class)
+
+@HiltAndroidTest
 class SignInActivityTest {
+    val scenarioRule = ActivityScenarioRule(SignInActivity::class.java)
 
     @get:Rule
-    var testRule = ActivityScenarioRule<SignInActivity>(SignInActivity::class.java)
+    val testRule: RuleChain = RuleChain.outerRule(HiltAndroidRule(this))
+        .around(scenarioRule)
 
     @Test
     fun testSignInButton() {
@@ -26,8 +33,12 @@ class SignInActivityTest {
 
     @Test
     fun testOpenCaptionSettings() {
+        Intents.init()
         Thread.sleep(4000) // wait for pop-up
         onView(withId(android.R.id.button1)).perform(click())
+        Intents.intended(IntentMatchers.hasAction(Settings.ACTION_CAPTIONING_SETTINGS))
+
+        Intents.release()
     }
 }
 
