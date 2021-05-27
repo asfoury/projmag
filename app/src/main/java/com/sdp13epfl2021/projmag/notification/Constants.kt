593c3f7dd77a6329e0cb53c7bc838d9f0fa53a1e
Giovanni Michel Igowa <giovanni.igowa@epfl.ch>
2021-05-27 17:57:36 +0200

    fix

--- app/src/main/java/com/sdp13epfl2021/projmag/notification/Constants.kt
+++ app/src/main/java/com/sdp13epfl2021/projmag/notification/Constants.kt
@@ -1,5 +1,5 @@
 package com.sdp13epfl2021.projmag.notification
 
 const val BASE_URL = "https://fcm.googleapis.com"
-const val SERVER_KEY= "AAAAwNSBlt8:APA91bFiQiRwZ7tdq2Y351D2ApIZEIQ5oLVTD16Vz78PBAnzAHZqGAZu6qYz0SVWVkD_UO9YZLzhMn7J87XIwMORHD-vP3oPdVvbkqJGdWGBjjnfqyHlsmpPaZdVFhY5_yJ2m6tBkbql"
+const val SERVER_KEY= "AAAAwNSBlt8:APA91bH7r-mQhoX9-KzML9N_86Hq1N6nZFO-cONdw7ghuG8SJHAmV4A3rZ-_TR8PEhxxGTGftPzThNVpYVT9_GF_a7PLobSobUdJ0pTSnUh5VvUl_p53Co-golonO3RhaXYzhUvJgSs2"
 const val  CONTENT_TYPE = "application/json"
