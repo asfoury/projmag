5559084648b5c85882b5aa98a62c7f0ec6d84960
Giovanni Michel Igowa <giovanni.igowa@epfl.ch>
2021-05-27 17:44:32 +0200

    secure and change the server key

--- app/src/main/java/com/sdp13epfl2021/projmag/notification/Constants.kt
+++ app/src/main/java/com/sdp13epfl2021/projmag/notification/Constants.kt
@@ -1,10 +1,5 @@
 package com.sdp13epfl2021.projmag.notification
 
-class Constants {
-    companion object{
-        const val BASE_URL = "https://fcm.googleapis.com"
-        const val SERVER_KEY= "AAAAwNSBlt8:APA91bHNLxj_wt6HCQ9Lfk3_6sSYIYHxfSRA7qgR_yZP-zCYxpgvBHlttayDPn20zKundac4-3RmyrcNwJPZagYLLuXOVOKljgVLRmIEZbLYODAXT1RblGZlddwa4U-SmCkURBlVs6TC"
-        const val  CONTENT_TYPE = "application/json"
-
-    }
-}
+const val BASE_URL = "https://fcm.googleapis.com"
+const val SERVER_KEY= "AAAAwNSBlt8:APA91bFiQiRwZ7tdq2Y351D2ApIZEIQ5oLVTD16Vz78PBAnzAHZqGAZu6qYz0SVWVkD_UO9YZLzhMn7J87XIwMORHD-vP3oPdVvbkqJGdWGBjjnfqyHlsmpPaZdVFhY5_yJ2m6tBkbql"
+const val  CONTENT_TYPE = "application/json"
--- app/src/main/java/com/sdp13epfl2021/projmag/notification/NotificationAPI.kt
+++ app/src/main/java/com/sdp13epfl2021/projmag/notification/NotificationAPI.kt
@@ -1,15 +1,17 @@
 package com.sdp13epfl2021.projmag.notification
 
-import com.sdp13epfl2021.projmag.notification.Constants.Companion.CONTENT_TYPE
-import com.sdp13epfl2021.projmag.notification.Constants.Companion.SERVER_KEY
+
+
 import okhttp3.ResponseBody
 import retrofit2.Response
 import retrofit2.http.Body
 import retrofit2.http.Headers
 import retrofit2.http.POST
 
+
 interface NotificationAPI {
-    @Headers("Authorization: key=$SERVER_KEY","Content-Type:$CONTENT_TYPE")
+    @Headers("Authorization:key=$SERVER_KEY","Content-Type:$CONTENT_TYPE")
+    //    @Headers("Authorization:key=$Resources.getSystem().getString(R.string.projmag_api_key_1)","Content-Type:$CONTENT_TYPE")
     @POST("fcm/send")
     suspend fun postNotification(
         @Body notification: PushNotification
