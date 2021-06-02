88c2fd1446d55488ce80e36bf082f8be207f8fba
Giovanni Michel Igowa <giovanni.igowa@epfl.ch>
2021-06-02 13:48:17 +0200

    fix test

--- app/src/main/java/com/sdp13epfl2021/projmag/notification/NotificationAPI.kt
+++ app/src/main/java/com/sdp13epfl2021/projmag/notification/NotificationAPI.kt
@@ -2,7 +2,7 @@ package com.sdp13epfl2021.projmag.notification
 
 
 
-import com.sdp13epfl2021.projmag.BuildConfig.SERVER_KEY_API
+import com.sdp13epfl2021.projmag.BuildConfig.API_KEY
 import okhttp3.ResponseBody
 import retrofit2.Response
 import retrofit2.http.Body
@@ -11,7 +11,7 @@ import retrofit2.http.POST
 
 
 interface NotificationAPI {
-    @Headers("Authorization:key=$SERVER_KEY_API","Content-Type:$CONTENT_TYPE")
+    @Headers("Authorization:key=$API_KEY","Content-Type:$CONTENT_TYPE")
     //    @Headers("Authorization:key=$Resources.getSystem().getString(R.string.projmag_api_key_1)","Content-Type:$CONTENT_TYPE")
     @POST("fcm/send")
     suspend fun postNotification(
