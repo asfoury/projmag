71f6800dc40f6829c8362ab267cb66aa1c502882
Giovanni Michel Igowa <giovanni.igowa@epfl.ch>
2021-06-03 09:23:49 +0200

    change key

--- app/src/main/java/com/sdp13epfl2021/projmag/notification/NotificationAPI.kt
+++ app/src/main/java/com/sdp13epfl2021/projmag/notification/NotificationAPI.kt
@@ -2,7 +2,8 @@ package com.sdp13epfl2021.projmag.notification
 
 
 
-import com.sdp13epfl2021.projmag.BuildConfig.API_KEY
+
+import com.sdp13epfl2021.projmag.notification.Cons.Companion.SERVER_KEY
 import okhttp3.ResponseBody
 import retrofit2.Response
 import retrofit2.http.Body
@@ -11,7 +12,7 @@ import retrofit2.http.POST
 
 
 interface NotificationAPI {
-    @Headers("Authorization:key=$API_KEY","Content-Type:$CONTENT_TYPE")
+    @Headers("Authorization:key=$SERVER_KEY","Content-Type:$CONTENT_TYPE")
     //    @Headers("Authorization:key=$Resources.getSystem().getString(R.string.projmag_api_key_1)","Content-Type:$CONTENT_TYPE")
     @POST("fcm/send")
     suspend fun postNotification(
